5fa5fb644df5c5508537f68d4d26ab3809c40b1a
Alina Kasiuk <37597014+AlinaKasiuk@users.noreply.github.com>
2020-10-09 19:48:12 +0200

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -13,25 +13,20 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from alinakasiuk.github.io";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+	$message ='<table style="width:100%">
+        	<tbody><tr>
+        	        <td>'.$name.'</td>
+        	</tr>
+       	 <tr><td>Email: '.$email.'</td></tr>
+	</tbody></table>';
+	if (@mail($to, $email, $message, $headers))
+	{
+   		 echo 'The message has been sent.';
+	}else{
+  		  echo 'failed';
+	}
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
 
 ?>
